2237646c7742901d5d49189ae0500c1bfd6dc199
kunaljainSgit <66411187+kunaljainSgit@users.noreply.github.com>
2020-10-03 01:48:46 -0400

    Update program.c

--- program.c
+++ program.c
@@ -2,15 +2,17 @@
 long int multiplyNumbers(int n);
 int main() {
     int n;
-    printf("Enter a positive integer: ");
+    printf("Enter a whole number: ");
     scanf("%d",&n);
     printf("Factorial of %d = %ld", n, multiplyNumbers(n));
     return 0;
 }
 
 long int multiplyNumbers(int n) {
+    if(n==0)
+        return 1;
     if (n>=1)
         return n*multiplyNumbers(n-1);
-    else
-        return 1;
+    else   
+	printf("you entered something wrong\n") ;       
 }
