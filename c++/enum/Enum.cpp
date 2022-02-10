3caaf0fb4a24ff87f898180b2bf8d2c8807decd7
Tech Rick <richubini101@gmail.com>
2022-02-10 18:06:15 +0530

    spliting a struct test

--- c++/enum/Enum.cpp
+++ c++/enum/Enum.cpp
@@ -2,17 +2,42 @@
 #include<conio.h>
 using namespace std;
 
-typedef struct details{
+typedef struct Sensor{
     int number;
-    char name[20];
+    float s;
     char div;
-}detail;
+}SensorValues;
 
+uint8_t *bufferr[sizeof(SensorValues)];
+
+void sendValues(SensorValues *values)
+{
+    writeBytes((uint8_t*)values, sizeof(SensorValues));
+}
+
+void writeBytes(uint8_t *buf, uint8_t len)
+{
+    std::cout<<"Writing values";
+    //Wire.beginTransmission(EDGE_I2C_ADDR);
+
+    for(uint8_t i = 0; i < len; i++) {
+        bufferr[i] = &buf[i];
+        std::cout<<buf[i]<< " "; 
+       // Wire.write(buf[i]);
+    }
+
+    std::cout<<endl;
+
+    //Wire.endTransmission();
+}
 
 
 int main()
 {   
-    detail val1;
-    
+    SensorValues val1;
+    val1.number=10;
+    val1.s=0.5;
+    val1.div ='a';
+    sendValues(&val1);
     getch();
 }
